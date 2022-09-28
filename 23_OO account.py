56854caaada940427924effdd8fcdc13efb7ae67
Peebs <blank-p@outlook.com>
2022-09-28 11:07:46 -0500

    account

---
+++ 23_OO account.py
@@ -0,0 +1,59 @@
+# Account class
+
+
+class Account():
+
+    def __init__(self, name, balance, password):
+        self.name = name
+        self.balance = int(balance)
+        self.password = password
+   
+    def deposit(self, amountToDeposit, password):
+        if password != self.password:
+            print('Sorry, incorrect password')
+            return None
+
+        if amountToDeposit < 0:
+            print('You cannot deposit a negative amount')
+            return None
+
+        self.balance = self.balance + amountToDeposit
+        return self.balance
+        
+    def withdraw(self, amountToWithdraw, password):
+        if password != self.password:
+            print('Incorrect password for this account')
+            return None
+
+        if amountToWithdraw < 0:
+            print('You cannot withdraw a negative amount')
+            return None
+
+        if amountToWithdraw > self.balance:
+            print('You cannot withdraw more than you have in your account')
+            return None
+
+        self.balance = self.balance - amountToWithdraw
+        return self.balance
+
+    def getBalance(self, password):
+        if password != self.password:
+            print('Sorry, incorrect password')
+            return None
+        return self.balance
+
+    def show(self):
+        print('       Name:', self.name)
+        print('       Balance:', self.balance)
+        print('       Password:', self.password)
+        print()
+
+
+# Error handling is minimal here, only print an error message and return None
+
+
+oAccount = Account('Joe Schmoe', 1000, 'magic')
+newBalance = oAccount.deposit(500, 'magic')
+oAccount.withdraw(250, 'magic')
+currentBalance = oAccount.getBalance('magic')
+print('curren balane:', currentBalance)
