8498cf7c3caca85a3e5f6fa7e38216cff9a9451e
Peebs <blank-p@outlook.com>
2022-09-28 11:54:53 -0500

    file names, account, import

--- 02_Account/_02_account_OOP.py
+++ 02_Account/_02_account_OOP.py
@@ -50,10 +50,3 @@ class Account():
 
 
 # Error handling is minimal here, only print an error message and return None
-
-
-oAccount = Account('Joe Schmoe', 1000, 'magic')
-newBalance = oAccount.deposit(500, 'magic')
-oAccount.withdraw(250, 'magic')
-currentBalance = oAccount.getBalance('magic')
-print('curren balane:', currentBalance)
