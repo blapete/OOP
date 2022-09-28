94b2543b6f8d669ec54c87728331eb6624f63da2
Peebs <blank-p@outlook.com>
2022-09-28 11:58:01 -0500

    git from top level dir

--- 02_Account/_03_import_account_OOP.py
+++
@@ -1,9 +0,0 @@
-from _02_account_OOP import *
-
-
-
-oAccount = Account('Joe Schmoe', 1000, 'magic')
-newBalance = oAccount.deposit(500, 'magic')
-oAccount.withdraw(250, 'magic')
-currentBalance = oAccount.getBalance('magic')
-print('current balance:', currentBalance)
